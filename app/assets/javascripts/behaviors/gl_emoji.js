d08e1efbff2b3783bfc161af7bc2c4d37cad6841
Lukas Eipert <leipert@gitlab.com>
2018-10-25 10:22:22 +0200

    Use WebComponents v2 for gl-emoji element
    Replaces deprecated `document.registerElement` (WebComponents v1) which
    is only supported by Chrome and Opera without polyfill with
    `window.customElements.define` (WebComponents v2) which is supported by
    Firefox and Safari as well. Polyfill for other Browsers is provided.

--- app/assets/javascripts/behaviors/gl_emoji.js
+++ app/assets/javascripts/behaviors/gl_emoji.js
@@ -1,51 +1,57 @@
-import installCustomElements from 'document-register-element';
+import '@webcomponents/webcomponentsjs/webcomponents-loader';
 import isEmojiUnicodeSupported from '../emoji/support';
 
-installCustomElements(window);
-
 export default function installGlEmojiElement() {
-  const GlEmojiElementProto = Object.create(HTMLElement.prototype);
-  GlEmojiElementProto.createdCallback = function createdCallback() {
-    const emojiUnicode = this.textContent.trim();
-    const { name, unicodeVersion, fallbackSrc, fallbackSpriteClass } = this.dataset;
+  class GlEmojiElement extends HTMLElement {
+    constructor() {
+      super();
+      const emojiUnicode = this.textContent.trim();
+      const { name, unicodeVersion, fallbackSrc, fallbackSpriteClass } = this.dataset;
 
-    const isEmojiUnicode =
-      this.childNodes &&
-      Array.prototype.every.call(this.childNodes, childNode => childNode.nodeType === 3);
-    const hasImageFallback = fallbackSrc && fallbackSrc.length > 0;
-    const hasCssSpriteFalback = fallbackSpriteClass && fallbackSpriteClass.length > 0;
+      const isEmojiUnicode =
+        this.childNodes &&
+        Array.prototype.every.call(this.childNodes, childNode => childNode.nodeType === 3);
 
-    if (emojiUnicode && isEmojiUnicode && !isEmojiUnicodeSupported(emojiUnicode, unicodeVersion)) {
-      // CSS sprite fallback takes precedence over image fallback
-      if (hasCssSpriteFalback) {
-        if (!gon.emoji_sprites_css_added && gon.emoji_sprites_css_path) {
-          const emojiSpriteLinkTag = document.createElement('link');
-          emojiSpriteLinkTag.setAttribute('rel', 'stylesheet');
-          emojiSpriteLinkTag.setAttribute('href', gon.emoji_sprites_css_path);
-          document.head.appendChild(emojiSpriteLinkTag);
-          gon.emoji_sprites_css_added = true;
+      if (
+        emojiUnicode &&
+        isEmojiUnicode &&
+        !isEmojiUnicodeSupported(emojiUnicode, unicodeVersion)
+      ) {
+        const hasCssSpriteFalback = fallbackSpriteClass && fallbackSpriteClass.length > 0;
+
+        // CSS sprite fallback takes precedence over image fallback
+        if (hasCssSpriteFalback) {
+          if (!gon.emoji_sprites_css_added && gon.emoji_sprites_css_path) {
+            const emojiSpriteLinkTag = document.createElement('link');
+            emojiSpriteLinkTag.setAttribute('rel', 'stylesheet');
+            emojiSpriteLinkTag.setAttribute('href', gon.emoji_sprites_css_path);
+            document.head.appendChild(emojiSpriteLinkTag);
+            gon.emoji_sprites_css_added = true;
+          }
+          // IE 11 doesn't like adding multiple at once :(
+          this.classList.add('emoji-icon');
+          this.classList.add(fallbackSpriteClass);
+        } else {
+          const hasImageFallback = fallbackSrc && fallbackSrc.length > 0;
+
+          import(/* webpackChunkName: 'emoji' */ '../emoji')
+            .then(({ emojiImageTag, emojiFallbackImageSrc }) => {
+              if (hasImageFallback) {
+                this.innerHTML = emojiImageTag(name, fallbackSrc);
+              } else {
+                const src = emojiFallbackImageSrc(name);
+                this.innerHTML = emojiImageTag(name, src);
+              }
+            })
+            .catch(() => {
+              // do nothing
+            });
         }
-        // IE 11 doesn't like adding multiple at once :(
-        this.classList.add('emoji-icon');
-        this.classList.add(fallbackSpriteClass);
-      } else {
-        import(/* webpackChunkName: 'emoji' */ '../emoji')
-          .then(({ emojiImageTag, emojiFallbackImageSrc }) => {
-            if (hasImageFallback) {
-              this.innerHTML = emojiImageTag(name, fallbackSrc);
-            } else {
-              const src = emojiFallbackImageSrc(name);
-              this.innerHTML = emojiImageTag(name, src);
-            }
-          })
-          .catch(() => {
-            // do nothing
-          });
       }
     }
-  };
+  }
 
-  document.registerElement('gl-emoji', {
-    prototype: GlEmojiElementProto,
+  window.WebComponents.waitFor(() => {
+    window.customElements.define('gl-emoji', GlEmojiElement);
   });
 }
