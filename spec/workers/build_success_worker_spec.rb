1943ac401993ae991dad27e300b2cd1a9928bbbf
Shinya Maeda <shinya@gitlab.com>
2018-10-26 16:37:10 +0900

    Remove empty spec

--- spec/workers/build_success_worker_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'spec_helper'
-
-# rubocop: disable RSpec/EmptyExampleGroup
-describe BuildSuccessWorker do
-  describe '#perform' do
-    # no-op
-  end
-end
-# rubocop: enable RSpec/EmptyExampleGroup
