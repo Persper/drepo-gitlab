aecccfb5118c8982db3ba502fdf37b5e639fbfc6
Shinya Maeda <shinya@gitlab.com>
2018-11-02 14:42:24 +0900

    Fix fill empty finished at migration

--- db/post_migrate/20181030135124_fill_empty_finished_at_in_deployments.rb
+++ db/post_migrate/20181030135124_fill_empty_finished_at_in_deployments.rb
@@ -4,6 +4,7 @@ class FillEmptyFinishedAtInDeployments < ActiveRecord::Migration
   include Gitlab::Database::MigrationHelpers
 
   DOWNTIME = false
+  DEPLOYMENT_STATUS_SUCCESS = 2 # Equivalent to Deployment.statuses[:success]
 
   class Deployments < ActiveRecord::Base
     self.table_name = 'deployments'
@@ -14,6 +15,7 @@ class FillEmptyFinishedAtInDeployments < ActiveRecord::Migration
   def up
     FillEmptyFinishedAtInDeployments::Deployments
       .where('finished_at IS NULL')
+      .where('status = ?', DEPLOYMENT_STATUS_SUCCESS)
       .each_batch(of: 10_000) do |relation|
       relation.update_all('finished_at=created_at')
     end
