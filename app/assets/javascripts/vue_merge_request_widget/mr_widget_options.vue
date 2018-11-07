2dce46e941eb55dc55f3d85984298d7a9654e2cc
Simon Knox <psimyn@gmail.com>
2018-11-07 20:42:52 +1100

    Fix karma - add missing prop to template

--- app/assets/javascripts/vue_merge_request_widget/mr_widget_options.vue
+++ app/assets/javascripts/vue_merge_request_widget/mr_widget_options.vue
@@ -312,7 +312,7 @@ export default {
       :key="`pre-merge-deploy-${deployment.id}`"
       class="js-pre-merge-deploy"
       :deployment="deployment"
-      :show-metrics="mr.isMergedState"
+      :show-metrics="false"
     />
     <div class="mr-section-container">
       <grouped-test-reports-app
@@ -367,6 +367,7 @@ export default {
         v-for="postMergeDeployment in mr.postMergeDeployments"
         :key="`post-merge-deploy-${postMergeDeployment.id}`"
         :deployment="postMergeDeployment"
+        :show-metrics="true"
         class="js-post-deployment"
       />
     </template>
