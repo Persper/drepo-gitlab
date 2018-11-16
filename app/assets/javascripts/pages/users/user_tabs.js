171e5e253310e566e3304c08f90a15465091b53c
Martin Wortschack <mwortschack@gitlab.com>
2018-11-16 13:54:25 +0100

    Remove irrelevant parameter

--- app/assets/javascripts/pages/users/user_tabs.js
+++ app/assets/javascripts/pages/users/user_tabs.js
@@ -186,7 +186,7 @@ export default class UserTabs {
       requestParams: { limit: 10 },
     });
     UserTabs.renderMostRecentBlocks('#js-overview .projects-block', {
-      requestParams: { limit: 10, skip_pagination: true, compact_mode: true },
+      requestParams: { limit: 10, skip_pagination: true },
     });
 
     this.loaded.overview = true;
