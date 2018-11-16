18e6b824200b3e9ef0d958f5308d3ee6a6885cbc
Sanad Liaquat <sliaquat@gitlab.com>
2018-11-16 18:41:53 +0500

    code for ce port

--- qa/qa/resource/user.rb
+++ qa/qa/resource/user.rb
@@ -76,6 +76,17 @@ module QA
         }
       end
 
+      def self.fabricate_or_use(username, password)
+        if !Runtime::Env.signup_disabled?
+          self.fabricate!
+        else
+          self.new.tap do |user|
+            user.username = username
+            user.password = password
+          end
+        end
+      end
+
       private
 
       def fetch_id(username)
