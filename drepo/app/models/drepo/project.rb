c72359e66fc756766e8fa2a9e417b1c909320205
jingyang <simple.continue@gmail.com>
2018-12-27 20:12:59 +0800

    Add drepo export

--- drepo/app/models/drepo/project.rb
+++ drepo/app/models/drepo/project.rb
@@ -15,5 +15,19 @@ module Drepo
       }
       Snapshots::CreateService.new(current_user, params).execute
     end
+
+    def drepo_import_export_shared
+      @drepo_import_export_shared ||= Drepo::ImportExport::Shared.new(self)
+    end
+
+    def add_drepo_export_job(current_user:, after_export_strategy: nil, params: {})
+      job_id = Drepo::ProjectExportWorker.perform_async(current_user.id, self.id, after_export_strategy, params)
+
+      if job_id
+        Rails.logger.info "Drepo Export job started for project ID #{self.id} with job ID #{job_id}"
+      else
+        Rails.logger.error "Drepo Export job failed to start for project ID #{self.id}"
+      end
+    end
   end
 end
