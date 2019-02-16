4d0610dafa0e056f6f1dcf37a86d40a05184331f
B1nj0y <huibin@meri.co>
2019-02-16 14:49:57 +0800

    add tabs in project drepo sync page

--- drepo/app/assets/javascripts/pages/projects/drepo_syncs/new/index.js
+++ drepo/app/assets/javascripts/pages/projects/drepo_syncs/new/index.js
@@ -1,6 +1,13 @@
 import initEtherWallet from '../../../../ether_wallet/index';
+import MergeRequest from '~/merge_request';
 
 document.addEventListener('DOMContentLoaded', () => {
   const etherWalletElement = document.getElementById('ether_wallet');
   initEtherWallet(etherWalletElement);
+
+  const mrNewSubmitNode = document.querySelector('.js-merge-request-new-submit');
+  // eslint-disable-next-line no-new
+  new MergeRequest({
+    action: mrNewSubmitNode.dataset.mrSubmitAction,
+  });
 });
--- drepo/app/controllers/projects/drepo_syncs_controller.rb
+++ drepo/app/controllers/projects/drepo_syncs_controller.rb
@@ -2,5 +2,8 @@ class Projects::DrepoSyncsController < Projects::ApplicationController
   before_action :authenticate_user!
 
   def new
+    @commits = ["xxx"]
+    @pipelines = []
+    @total_commit_count = 5
   end
 end
